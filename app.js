2f8047fd44d23c75498590ae11e01003d4e531ff
wupengforIT <wupengforIT@163.com>
2018-10-27 17:45:48 +0800

    网站的基本页面架构完成

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var http = require('http');
 var fs = require('fs');
-http.createServer(function (request,response) {
-    if(request.url.indexOf('/public/') === 0){
-        fs.readFile(request.url,function (error,content) {
-            if(error){
-                response.end('404 not found');
-            }
-            response.end(content);
-        })
+
+function readFile(path, response) {
+    fs.readFile(path, function (error, content) {
+        console.log("path：" + path);
+        if (error) {
+            response.end('404 not found');
+        }
+        response.end(content);
+    });
+}
+
+http.createServer(function (request, response) {
+    if (request.url === '/') {
+        readFile('./views/index.html', response);
+    } else if (request.url.indexOf('/public/') === 0) {
+        readFile('.' + request.url, response);
+    } else if (request.url === '/post') {
+        readFile('./views/post.html', response);
+    } else {
+        readFile('./views/404.html', response);
     }
-
-
-}).listen('3000',function () {
+}).listen('3000', function () {
     console.log('服务已经启动')
 });
